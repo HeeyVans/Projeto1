3cb99d3abd5569eab11d00dc728585868112373f
Vanessa Cristina <vanessa.cs.oliveira@gmail.com>
2019-02-07 00:00:46 -0300

    Classe treino

---
+++ src/dados/Treino.java
@@ -0,0 +1,13 @@
+package dados;
+
+public class Treino {
+	
+	private String exercicio;
+	private String obs;
+	private int serie;
+	private int tempoDescanso;
+	private double porcentagem;
+	private Instrutor instrutor;
+	private Cliente cliente;
+
+}
