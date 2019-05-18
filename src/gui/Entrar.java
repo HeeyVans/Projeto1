e6fb2767de7683e119c8cb8152a3f69d3b717811
Vanessa Cristina <vanessa.cs.oliveira@gmail.com>
2019-05-18 19:00:48 -0300

    att tela de entrar

--- src/gui/Entrar.java
+++ src/gui/Entrar.java
@@ -12,10 +12,15 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 import javax.swing.JTextPane;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
 
 public class Entrar {
 
 	private JFrame TelaDeEntrada;
+	private JTextField txtMatrcula;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -55,41 +60,78 @@ public class Entrar {
 	 */
 	private void initialize() {
 		TelaDeEntrada = new JFrame();
-		TelaDeEntrada.setTitle("Tela de Entrada");
+		TelaDeEntrada.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Ricardo\\Documents\\UFRPE\\PROGRAMA\u00C7\u00C3O\\JAVA\\Projeto - Programa\u00E7\u00E3o\\biceps png.png"));
+		TelaDeEntrada.setTitle("Sistema de Login da MFit");
 		TelaDeEntrada.getContentPane().setBackground(Color.WHITE);
 		TelaDeEntrada.getContentPane().setLayout(null);
 		
 		JTextPane txtpnCliqueEmUma = new JTextPane();
 		txtpnCliqueEmUma.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 11));
-		txtpnCliqueEmUma.setText("Clique em uma das op\u00E7\u00F5es acima para entrar no sistema");
-		txtpnCliqueEmUma.setBounds(20, 399, 349, 34);
+		txtpnCliqueEmUma.setText("Insira suas informações acima para entrar no sistema");
+		txtpnCliqueEmUma.setBounds(109, 391, 349, 34);
 		TelaDeEntrada.getContentPane().add(txtpnCliqueEmUma);
 		
 		JPanel panel = new JPanel();
-		panel.setBackground(Color.BLUE);
-		panel.setBounds(0, 0, 369, 92);
+		panel.setBackground(new Color(0, 128, 0));
+		panel.setBounds(0, 0, 519, 98);
 		TelaDeEntrada.getContentPane().add(panel);
 		panel.setLayout(null);
 		
-		JButton btnNewButton = new JButton("Instrutor");
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		btnNewButton.setBounds(118, 241, 135, 40);
-		TelaDeEntrada.getContentPane().add(btnNewButton);
+		JLabel lblBemvindoAMfit = new JLabel("Bem-vindo a MFit");
+		lblBemvindoAMfit.setForeground(new Color(255, 255, 255));
+		lblBemvindoAMfit.setHorizontalAlignment(SwingConstants.CENTER);
+		lblBemvindoAMfit.setFont(new Font("Tahoma", Font.BOLD, 24));
+		lblBemvindoAMfit.setBounds(157, 37, 209, 29);
+		panel.add(lblBemvindoAMfit);
 		
-		JButton btnAdministrador = new JButton("Administrador");
-		btnAdministrador.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		btnAdministrador.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnAdministrador.setBounds(118, 168, 135, 40);
-		TelaDeEntrada.getContentPane().add(btnAdministrador);
+		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar.setBackground(new Color(0, 128, 0));
+		btnEntrar.setForeground(new Color(255, 255, 255));
+		btnEntrar.setIcon(new ImageIcon("C:\\Users\\Ricardo\\Documents\\UFRPE\\PROGRAMA\u00C7\u00C3O\\JAVA\\Projeto - Programa\u00E7\u00E3o\\login.png"));
+		btnEntrar.setFont(new Font("Arial", Font.BOLD, 14));
+		btnEntrar.setBounds(246, 304, 114, 34);
+		TelaDeEntrada.getContentPane().add(btnEntrar);
 		
-		JButton btnAluno = new JButton("Aluno");
-		btnAluno.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		btnAluno.setBounds(118, 310, 135, 40);
-		TelaDeEntrada.getContentPane().add(btnAluno);
-		TelaDeEntrada.setBounds(100, 100, 385, 472);
+		JButton btnSair = new JButton("Sair");
+		btnSair.setForeground(new Color(255, 255, 255));
+		btnSair.setBackground(new Color(0, 128, 0));
+		btnSair.setIcon(new ImageIcon("C:\\Users\\Ricardo\\Documents\\UFRPE\\PROGRAMA\u00C7\u00C3O\\JAVA\\Projeto - Programa\u00E7\u00E3o\\sair-menu-2.png"));
+		btnSair.setFont(new Font("Arial", Font.BOLD, 14));
+		btnSair.setBounds(383, 304, 114, 34);
+		TelaDeEntrada.getContentPane().add(btnSair);
+		
+		txtMatrcula = new JTextField();
+		txtMatrcula.setColumns(10);
+		txtMatrcula.setBounds(310, 231, 187, 34);
+		TelaDeEntrada.getContentPane().add(txtMatrcula);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(310, 172, 187, 34);
+		TelaDeEntrada.getContentPane().add(textField);
+		
+		JLabel lblCpf = new JLabel("CPF:");
+		lblCpf.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblCpf.setBounds(269, 169, 42, 34);
+		TelaDeEntrada.getContentPane().add(lblCpf);
+		
+		JLabel lblMatrcula = new JLabel("Matr\u00EDcula:");
+		lblMatrcula.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblMatrcula.setBounds(231, 238, 90, 14);
+		TelaDeEntrada.getContentPane().add(lblMatrcula);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Ricardo\\Documents\\UFRPE\\PROGRAMA\u00C7\u00C3O\\JAVA\\Projeto - Programa\u00E7\u00E3o\\pessoas-saudaveis-carregando-icones-diferentes_53876-43069 novo.jpg"));
+		lblNewLabel.setBounds(10, 114, 211, 255);
+		TelaDeEntrada.getContentPane().add(lblNewLabel);
+		
+		JLabel lblEsqueciASenha = new JLabel("Esqueci minha senha");
+		lblEsqueciASenha.setForeground(new Color(32, 178, 170));
+		lblEsqueciASenha.setHorizontalAlignment(SwingConstants.CENTER);
+		lblEsqueciASenha.setFont(new Font("Arial", Font.BOLD, 11));
+		lblEsqueciASenha.setBounds(320, 276, 145, 14);
+		TelaDeEntrada.getContentPane().add(lblEsqueciASenha);
+		TelaDeEntrada.setBounds(100, 100, 535, 454);
 		TelaDeEntrada.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
