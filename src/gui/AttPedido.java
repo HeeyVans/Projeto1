d59465eaf4005f63d56eb3f7bab442ed6cb15a43
Vanessa Cristina <vanessa.cs.oliveira@gmail.com>
2019-06-01 09:30:26 -0300

    Atualização tela attPedido

--- src/gui/AttPedido.java
+++ src/gui/AttPedido.java
@@ -17,6 +17,10 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.Toolkit;
+import java.awt.Font;
+import javax.swing.ImageIcon;
 
 public class AttPedido extends JFrame {
 
@@ -28,11 +32,22 @@ public class AttPedido extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException  ex) {
+            System.err.println(ex);
+        } 
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					AttPedido frame = new AttPedido();
 					frame.setVisible(true);
+					frame.setLocationRelativeTo(null);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -51,23 +66,30 @@ public class AttPedido extends JFrame {
 	 * Create the frame.
 	 */
 	public AttPedido() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(AttPedido.class.getResource("/imagens/biceps png.png")));
+		setTitle("Tela de Pedido - MFit");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 504, 348);
+		setBounds(100, 100, 496, 348);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lblDeseja = new JLabel("O que deve ser alterado?");
-		lblDeseja.setBounds(51, 33, 165, 14);
+		lblDeseja.setFont(new Font("Arial", Font.BOLD, 18));
+		lblDeseja.setBounds(51, 33, 245, 22);
 		contentPane.add(lblDeseja);
 		
 		textFieldAlterar = new JTextField();
-		textFieldAlterar.setBounds(51, 66, 245, 109);
+		textFieldAlterar.setBounds(51, 66, 311, 127);
 		contentPane.add(textFieldAlterar);
 		textFieldAlterar.setColumns(10);
 		
 		JButton btnEnviar = new JButton("Enviar Solicita\u00E7\u00E3o");
+		btnEnviar.setBackground(new Color(0, 128, 0));
+		btnEnviar.setForeground(new Color(255, 255, 255));
+		btnEnviar.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnEnviar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				boolean confirm;
@@ -95,10 +117,14 @@ public class AttPedido extends JFrame {
 				
 			}
 		});
-		btnEnviar.setBounds(51, 199, 165, 23);
+		btnEnviar.setBounds(51, 224, 143, 38);
 		contentPane.add(btnEnviar);
 		
 		JButton btnVoltar = new JButton("Voltar");
+		btnVoltar.setIcon(new ImageIcon(AttPedido.class.getResource("/imagens/gtkgobackltr_104397.png")));
+		btnVoltar.setBackground(new Color(0, 128, 0));
+		btnVoltar.setForeground(Color.WHITE);
+		btnVoltar.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnVoltar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				boolean confirm;
@@ -119,7 +145,7 @@ public class AttPedido extends JFrame {
 				}
 			}
 		});
-		btnVoltar.setBounds(51, 235, 89, 23);
+		btnVoltar.setBounds(219, 224, 143, 38);
 		contentPane.add(btnVoltar);
 	}
 }
