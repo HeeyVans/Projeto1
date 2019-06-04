1a74f56b4e5ff73bab0b7532c27049ffbf284304
Vanessa Cristina <vanessa.cs.oliveira@gmail.com>
2019-06-04 00:10:30 -0300

    -Testando o localDate na classe AtividadeDiaria

--- src/basicas/AtividadeDiaria.java
+++ src/basicas/AtividadeDiaria.java
@@ -1,9 +1,25 @@
 package basicas;
 
-import javafx.scene.chart.PieChart.Data;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class AtividadeDiaria {
 
 	Treino treino;
-	Data localDate;
+	LocalDate data;
+	
+	public static void main(String[] args) {
+		  
+	        //Pega a data atual e a hora
+		 	LocalDateTime agora = LocalDateTime.now();
+
+	        System.out.println("LocalDateTime antes de formatar: " + agora);
+
+	        DateTimeFormatter formatadorBarraEHora = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+	        System.out.println("LocalDateTime depois de formatar: " +agora.format(formatadorBarraEHora));
+
+
+	}
 }
